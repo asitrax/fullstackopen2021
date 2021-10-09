b9e44d065c4cff6672fe8378b039f1411f3b5240
Daniele Battaglia <13153332+asitrax@users.noreply.github.com>
2021-10-09 15:49:35 +0200

    Exercise 1.6 completed

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>{props.text} {props.value}</div>
+  )
+}
+
+const Statistics = (props) => {
+
+  return(
+    <div>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text='good' onClick={() => setGood(good + 1)} />
+      <Button text='neutral' onClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' onClick={() => setBad(bad + 1)}/>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
