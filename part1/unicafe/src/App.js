76a8a1a3336e9b42e057ac06f534f5edf73563cb
Daniele Battaglia <13153332+asitrax@users.noreply.github.com>
2021-10-09 16:08:13 +0200

    Exercises 1.8, 1.9 and 1.10 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -19,16 +19,23 @@ const Statistics = (props) => {
   const average = allFeedbacks > 0 ? (props.good - props.bad) / allFeedbacks : 0
   const positivePerc = allFeedbacks > 0 ? props.good / allFeedbacks : 0
 
-  return(
-    <div>
-      <StatisticLine text="good" value = {props.good} />
-      <StatisticLine text="neutral" value = {props.neutral} />
-      <StatisticLine text="bad" value = {props.bad} />
-      <StatisticLine text="all" value = {allFeedbacks} />
-      <StatisticLine text="average" value = {average} />
-      <StatisticLine text="positive" value = {`${positivePerc * 100} %`} />
-    </div>
+  if (allFeedbacks > 0) {
+    return(
+      <div>
+        <StatisticLine text="good" value = {props.good} />
+        <StatisticLine text="neutral" value = {props.neutral} />
+        <StatisticLine text="bad" value = {props.bad} />
+        <StatisticLine text="all" value = {allFeedbacks} />
+        <StatisticLine text="average" value = {average} />
+        <StatisticLine text="positive" value = {`${positivePerc * 100} %`} />
+      </div>
+    )
+  }
+  return (
+    <div>No feedback given</div>
   )
+
+  
 }
 
 const App = () => {
