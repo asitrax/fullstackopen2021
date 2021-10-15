1906b361fafdd9953fcefcfc2eb34c6f32509034
Daniele Battaglia <13153332+asitrax@users.noreply.github.com>
2021-10-15 17:57:56 +0200

    Exercises 2.2 and 2.3 completed

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -8,9 +8,9 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum = course.parts.reduce((prev, curr) => prev + curr.exercises, 0)
   return(
-    <p>Number of exercises {sum}</p>
+    <p>total of {sum} exercises</p>
   ) 
 }
 
@@ -35,6 +35,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
